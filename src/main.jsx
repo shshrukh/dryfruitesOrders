2cba7776ccf05be303c920434a60171b3e57d580
shshrukh <shshrukh.arbaz@gmail.com>
2026-01-23 19:51:20 +0500

    arranging repo structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import route from './Router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router ={route}>
+
+      </RouterProvider>
   </StrictMode>,
 )
