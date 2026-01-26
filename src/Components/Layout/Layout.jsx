1515ee7bc98b62353ff7c53d8daa0ccb765f268b
shshrukh <shshrukh.arbaz@gmail.com>
2026-01-26 19:03:43 +0500

    adding bestCardComponent

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
 
+
 const Layout = () => {
   return (
     <>  
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Hero from "../Components/Hero";
+import dummyJsons from '../Components/UI/utils/cardsProducts.json'
+import BestProducts from '../Components/UI/BestProducts';
 
 
 
 const Home = () => {
+  const data = dummyJsons;
   return (
     <div>
      <Hero />
-
+     
     </div>
   )
+
 }
 
 export default Home
